f5f47171a6499e07ba1a0f356d74b7b89910f441
Gagandeep Singh <gagan.sngh14@gmail.com>
2021-10-01 10:33:51 +0530

    further development

--- js/script.js
+++ js/script.js
@@ -1,31 +1,30 @@
 function myFunction() {
     var x = document.getElementById("nav-side-menu");
     var burger = document.getElementById("burger");
-
     var hey = document.getElementById("hey");
     var about = document.getElementById("about");
 
     if (x.style.display === "block") {
         x.style.display = "none";
-        burger.innerHTML='<i class="fas fa-bars"></i>';
+        burger.innerHTML = '<i class="fas fa-bars"></i>';
         burger.style.color = "var(--secondary-color)";
         burger.style.border = "1px solid var(--secondary-color)";
         burger.style.backgroundColor = "var(--primary-color)";
 
-        hey.classList.remove("blurry");
-        about.classList.remove("blurry");
-
     } else {
         x.style.display = "block";
         x.style.overflow = "hidden";
-        burger.innerHTML='<i class="fas fa-times"></i>';
+        burger.innerHTML = '<i class="fas fa-times"></i>';
         burger.style.color = "var(--white)";
         burger.style.border = "1px solid var(--white)";
         burger.style.backgroundColor = "var(--secondary-color)";
-
-        hey.className = "blurry";
-        about.className = "blurry";
     }
 }
 
-// document.getElementById("myDIV").className = "mystyle"; 
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+        $('header').addClass('darkHeader');
+    } else {
+        $('header').removeClass('darkHeader');
+    }
+});
